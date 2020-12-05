91574914a5a4317a824432f584a57ec1bb360a29
Alexander Hjelm <nick.nertova@gmail.com>
2020-12-05 19:35:54 +0100

    workbook I/O

--- finance.py
+++ finance.py
@@ -1,14 +1,45 @@
+import sys
 from openpyxl import load_workbook
 
-wb = load_workbook(filename='example-data.xlsx')
+class CostEntry:
 
-# List sheets available
-sheets = wb.get_sheet_names()
-print(sheets)
+    def __init__(self, datetime, cost, comment):
+        self.datetime = datetime
+        self.cost = cost
+        self.comment = comment
 
-# Load active sheet or named sheet
-sheet = wb.active
-# sheet = wb['User Information']
 
-# Read a specific cell
-print(sheet['D8'].value)
+# Read data file names
+input_filenames = []
+for i in range(1, len(sys.argv)-1):
+    input_filenames.append(sys.argv[i])
+
+output_filename = sys.argv[-1]
+wb_output = load_workbook(output_filename)
+sheet_out = wb_output.active
+
+for filename in input_filenames:
+    # Load xlsx workbook
+    wb_input = load_workbook(filename)
+
+    # List sheets available
+    #sheets = wb.get_sheet_names()
+    #print(sheets)
+
+    # Load active sheet or named sheet
+    sheet_in = wb_input.active
+
+    # sheet = wb['User Information']
+
+    # Iterate over rows
+    r = 1
+    while sheet_in['A'+str(r)].value != None:
+
+        # Read a specific cell
+        cost_entry = CostEntry(
+            sheet_in['A'+str(r)].value,
+            sheet_in['B'+str(r)].value,
+            sheet_in['C'+str(r)].value
+        )
+
+        r = r+1
